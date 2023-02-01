2f308f4cff8d2d5994551d45d46c4567efb54440
Shad <34666977+shadmoney@users.noreply.github.com>
2023-02-01 00:57:55 -0500

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,8 +11,8 @@ import "hardhat/console.sol";
 // This is the main building block for smart contracts.
 contract Token {
     // Some string type variables to identify the token.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+    string public name = "Decentralized Arcade Machine Token";
+    string public symbol = "DAM";
 
     // The fixed amount of tokens stored in an unsigned integer type variable.
     uint256 public totalSupply = 1000000;
